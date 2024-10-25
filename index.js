e72a8937df8b76c0e73e6ef69aa893871de0fb92
Your Name <you@example.com>
2024-10-25 17:41:37 +0530

    Card-animated

---
+++ index.js
@@ -0,0 +1,12 @@
+const sharebutton=document.querySelector(".button-1")
+const modal=document.querySelector(".modal");
+const crossbutton=document.querySelector(".crossbutton");
+sharebutton.addEventListener("click", function(e){
+    // modal.classList.toggle("modal")
+    modal.classList.toggle("modal-2")
+
+})
+crossbutton.addEventListener("click",function(e){
+    // modal.classList.toggle("modal")
+    modal.classList.toggle("modal-2")
+})
